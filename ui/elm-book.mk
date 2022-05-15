a7e8134effa16f072e77ec765ea8663d5129109c
Jaime Terreu <jaime@terreu.com>
2022-05-15 11:51:10 +0930

    feat: Make multiline for readability

--- ui/elm-book.mk
+++ ui/elm-book.mk
@@ -1,3 +1,4 @@
 .PHONY: elm-book-live
 elm-book-live:
-	ELMLIVE=$$(${Y} bin elm-live) && cd ui && $$ELMLIVE src/Main.elm --pushstate --port=8080
+	ELMLIVE=$$(${Y} bin elm-live) && cd ui && \
+					$$ELMLIVE src/Main.elm --pushstate --port=8080 -- --debug
